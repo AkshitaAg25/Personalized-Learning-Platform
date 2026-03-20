a34734606418b3d56f858f6b75ca81ad9b69e90d
AkshitaAg25 <akshita.agarwal_cs.aiml24@gla.ac.in>
2026-03-21 00:01:35 +0530

    .

--- templates/justcheckingnouse.py
+++
@@ -1,8 +0,0 @@
-from google import genai
-
-# Initialize client
-client = genai.Client(api_key="AIzaSyA1oqF9bFoWXpt9iRo_5dPM42CNnre3dsk")
-
-# Fetch and print available models
-for model in client.models.list():
-    print(f"Model ID: {model.name}")
